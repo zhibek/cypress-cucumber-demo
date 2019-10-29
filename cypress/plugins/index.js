57b62356779a14928592a71601f62c24fffc164e
John Levermore <john.levermore@zhibek.com>
2019-10-29 13:26:11 +0000

    Add linting.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,10 @@
+/*eslint-disable */
+// "cypress-cucumber-preprocessor" follows module/require style loading, so ignore lint
+
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
+
+/*eslint-enable */
